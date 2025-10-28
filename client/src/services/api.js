47697ef25e7ac6635205314682bfef7ae85feacc
em <emilyjiusa@gmail.com>
2025-10-28 13:10:22 -0500

    backend to frontend api connection

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+// axios instance for making API requests
 const api = axios.create({
     baseURL: 'http://localhost:8081/api',
     headers: {
@@ -8,6 +9,8 @@ const api = axios.create({
 });
 
 export const employeeAPI = {
+    // res: login employee with given password
+    // rep: backend returns employee data if valid password
     login: (password) => api.post('/cashier/login', { password }),
 };
 
--- server/models/employee.js
+++ server/models/employee.js
@@ -1,7 +1,8 @@
 const pool = require('../database');
 
 const Employee = {
-    //
+    // queries DB for employee matching given password
+    // returns first matching employee's data
     getEmployeePassword: async (password) => {
         const res = await pool.query(
             'SELECT id, password, first_name, last_name, role FROM Employee WHERE password = $1 LIMIT 1',
