b10f0c8f3790038767228abf32917ac180d716a1
Nikolai Steen <niksteen705@gmail.com>
2025-11-17 17:02:08 -0600

    Add Sprint 1 comment documentation to client/admin JSX files

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,12 @@
+// main.jsx
+// Entry point for the React app. Sets up StrictMode and renders the Client router.
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './components/cashier/css/main.css'
-import Client from './client.jsx'
+import './components/cashier/css/main.css'  // global stylesheet
+import Client from './client.jsx'           // main application router component
 
+// Render the root React component into the HTML element with id="root"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Client />
